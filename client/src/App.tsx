2402699847fbbe4c9a4cb272f4ba4816b94e2c8b
lloydrichards <lloyd.d.richards@gmail.com>
2024-10-03 18:55:01 +0200

    feat(client): :sparkles: add tailwind and shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
-import "./App.css";
 import { Login } from "./components/Login";
 import { Dashboard } from "./components/Dashboard";
 
 function App() {
   const [username, setUsername] = useState<string | null>(null);
 
-  return !username ? (
-    <>
-      <Login onSubmit={setUsername} />
-    </>
-  ) : (
-    <Dashboard username={username} />
+  return (
+    <main className="h-screen">
+      {!username ? (
+        <>
+          <Login onSubmit={setUsername} />
+        </>
+      ) : (
+        <Dashboard username={username} />
+      )}
+    </main>
   );
 }
 
