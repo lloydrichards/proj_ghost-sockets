42450ee6deb26061bfb197893b0ee9e54d4ae924
lloydrichards <lloyd.d.richards@gmail.com>
2024-10-03 12:28:29 +0200

    style(client): :lipstick: add other cursors from broadcast

--- client/src/components/Cursor.tsx
+++ client/src/components/Cursor.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { ReactNode, useMemo } from "react";
 import { useMousePosition } from "../hooks/use-mouse-position";
 import { useThrottle } from "../hooks/use-trottle";
 
 type CursorProps = {
-  onMove: (x: number, y: number) => void;
+  onMove?: (x: number, y: number) => void;
   client?: boolean;
   x?: number;
   y?: number;
+  children?: ReactNode;
+};
+
+const colors = [
+  "#1E90FF",
+  "#9B59B6",
+  "#00CED1",
+  "#2ECC71",
+  "#E67E22",
+  "#E74C3C",
+  "#F1C40F",
+  "#95A5A6",
+  "#D87093",
+  "#3CB371",
+];
+
+const randomColor = () => {
+  const rndIdx = Math.floor(Math.random() * colors.length);
+  return colors[rndIdx];
 };
 
 export const Cursor: React.FC<CursorProps> = ({
@@ -14,27 +33,36 @@ export const Cursor: React.FC<CursorProps> = ({
   client = false,
   x,
   y,
+  children,
 }) => {
   const mousePosition = useMousePosition();
-  const throttlePosition = useThrottle(mousePosition, 500);
+  const throttlePosition = useThrottle(mousePosition, 100);
 
   React.useEffect(() => {
     if (!client) return;
-    onMove(throttlePosition.x ?? 0, throttlePosition.y ?? 0);
+    onMove?.(throttlePosition.x ?? 0, throttlePosition.y ?? 0);
   }, [throttlePosition, client, onMove]);
+
+  const size = 24;
+  const xPos = x ?? throttlePosition.x ?? 0;
+  const yPos = y ?? throttlePosition.y ?? 0;
+  const color = useMemo(() => randomColor(), []);
 
   return (
     <div style={{ position: "fixed", top: 0, left: 0, pointerEvents: "none" }}>
       <div
         style={{
           position: "absolute",
-          left: x ?? mousePosition.x ?? 0,
-          top: y ?? mousePosition.y ?? 0,
-          width: 10,
-          height: 10,
-          backgroundColor: "red",
+          left: xPos - size / 2,
+          top: yPos - size / 2,
+          width: size,
+          height: size,
+          borderRadius: 99,
+          backgroundColor: color,
         }}
-      />
+      >
+        {children}
+      </div>
     </div>
   );
 };
--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,18 +1,52 @@
 import React, { useCallback } from "react";
 import useWebSocket, { ReadyState } from "react-use-websocket";
 import { Cursor } from "./Cursor";
+import { z } from "zod";
 
 type DashboardProps = {
   username: string;
 };
 
+const Payload = z.object({
+  type: z.string(),
+  payload: z.record(
+    z.string(),
+    z.object({
+      username: z.string(),
+      state: z.object({
+        x: z.number(),
+        y: z.number(),
+      }),
+    })
+  ),
+});
+
 export const Dashboard: React.FC<DashboardProps> = ({ username }) => {
+  const [otherCursors, setOtherCursors] = React.useState<
+    Record<string, { x: number; y: number }>
+  >({});
   const { sendMessage, lastJsonMessage, readyState } = useWebSocket(
     "ws://localhost:9000/ws",
     {
       queryParams: { username },
     }
   );
+
+  React.useEffect(() => {
+    if (lastJsonMessage) {
+      const parsed = Payload.safeParse(lastJsonMessage);
+      if (parsed.success) {
+        const { payload } = parsed.data;
+        setOtherCursors(
+          Object.fromEntries(
+            Object.values(payload)
+              .map(({ username, state }) => [username, state])
+              .filter(([id]) => id !== username)
+          )
+        );
+      }
+    }
+  }, [lastJsonMessage, username]);
 
   const connectionStatus = {
     [ReadyState.CONNECTING]: "Connecting",
@@ -43,6 +77,21 @@ export const Dashboard: React.FC<DashboardProps> = ({ username }) => {
       Welcome, {username}
       <p>{connectionStatus}</p>
       <Cursor client onMove={handleMessages} />
+      {Object.entries(otherCursors).map(([username, { x, y }]) => (
+        <Cursor key={username} x={x} y={y}>
+          <p
+            style={{
+              position: "absolute",
+              top: -16,
+              left: 32,
+              color: "white",
+              fontSize: 16,
+            }}
+          >
+            {username}
+          </p>
+        </Cursor>
+      ))}
     </div>
   );
 };
