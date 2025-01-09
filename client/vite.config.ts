0b8619cf7cd5f71438f1237689c529faeed8e206
lloydrichards <lloyd.d.richards@gmail.com>
2025-01-09 11:46:05 +0100

    refactor(client): :construction_worker: update docker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,6 +12,12 @@ export default defineConfig({
   },
   preview: {
     host: true,
-    port: +(process.env.PORT || 3000),
+    port: +(process.env.PORT || 8080),
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
   },
 });
